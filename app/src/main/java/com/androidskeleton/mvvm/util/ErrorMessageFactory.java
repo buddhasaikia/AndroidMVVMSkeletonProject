7a31f7f8e36c5b43c0da4d98303579e9ea52db0a
Buddha Saikia <13464551+buddhasaikia@users.noreply.github.com>
2018-11-26 18:46:55 +0530

    Code optimization on error factory class

--- app/src/main/java/com/androidskeleton/mvvm/util/ErrorMessageFactory.java
+++ app/src/main/java/com/androidskeleton/mvvm/util/ErrorMessageFactory.java
@@ -12,8 +12,8 @@ public class ErrorMessageFactory {
     public String getError(Throwable t) {
         t.printStackTrace();
         String msg;
-        if (t instanceof ConnectException | t instanceof UnknownHostException
-                | t instanceof NetworkErrorException) {
+        if (t instanceof ConnectException || t instanceof UnknownHostException
+                || t instanceof NetworkErrorException) {
             msg = "Could not connect to sever, Please check your internet connection or try again later.";
         } else if (t instanceof SocketTimeoutException || t instanceof NoRouteToHostException) {
             msg = "Something snapped. Please check internet connection and try again.";
